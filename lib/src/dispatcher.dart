58bd361f248301b86f819308cd039091e1e7e3c1
rxlabz <dev@rxlabz.com>
2016-12-19 15:35:29 +0100

    Feat : add an alternative request channel for non-reduced command ( i.e Firebase updates)

--- lib/src/dispatcher.dart
+++ lib/src/dispatcher.dart
@@ -6,11 +6,18 @@ import 'package:redarx/src/request.dart';
 class Dispatcher {
 
   /// stream of command requests : requests are added via the dispatch() method
-  final StreamController<Request> streamController = new StreamController();
-
+  final StreamController<Request> requestController = new StreamController<Request>.broadcast();
   /// dispatched [Stream]<[Request]>
-  Stream<Request> get request$ => streamController.stream;
+  Stream<Request> get request$ => requestController.stream;
+
+  /// stream of queries : queries are alternative messaging bus
+  /// it canbe used for non store commands like firebase requests
+  final StreamController<Request> queryController = new StreamController<Request>.broadcast();
+  Stream<Request> get querie$ => queryController.stream;
 
   /// add [Request] to stream
-  void dispatch(Request a) => streamController.add(a);
+  void dispatch(Request req) => requestController.add(req);
+
+  /// add [Request] to stream
+  void query(Request req) => queryController.add(req);
 }
